datawrapper/storybook-svelte2

Add `nextjs` framework

code/frameworks/nextjs/src/preset.ts
@@ -1,0 +1,85 @@
+// https://storybook.js.org/docs/react/addons/writing-presets
+import { dirname, join } from 'path';
+import { Options, PresetProperty } from '@storybook/core-common';
+import { TransformOptions } from '@babel/core';
+import { configureConfig } from './config/webpack';
+import { configureCss } from './css/webpack';
+import { configureImports } from './imports/webpack';
+import { configureRouting } from './routing/webpack';
+import { configureStyledJsx } from './styledJsx/webpack';
+import { configureStyledJsxTransforms } from './styledJsx/babel';
+import { configureImages } from './images/webpack';
+import { configureRuntimeNextjsVersionResolution } from './utils';
+import { FrameworkOptions, StorybookConfig } from './types';
+
+export const addons: PresetProperty<'addons', StorybookConfig> = [
+  dirname(require.resolve(join('@storybook/preset-react-webpack', 'package.json'))),
+  dirname(require.resolve(join('@storybook/react', 'package.json'))),
+  dirname(require.resolve(join('@storybook/builder-webpack5', 'package.json'))),
+];
+
+const defaultFrameworkOptions: FrameworkOptions = {};
+
+export const frameworkOptions = async (
+  _: never,
+  options: Options
+): Promise<StorybookConfig['framework']> => {
+  const config = await options.presets.apply<StorybookConfig['framework']>('framework');
+
+  if (typeof config === 'string') {
+    return {
+      name: config,
+      options: defaultFrameworkOptions,
+    };
+  }
+  if (typeof config === 'undefined') {
+    return {
+      name: require.resolve('@storybook/cra') as '@storybook/cra',
+      options: defaultFrameworkOptions,
+    };
+  }
+
+  return {
+    name: config.name,
+    options: {
+      ...defaultFrameworkOptions,
+      ...config.options,
+    },
+  };
+};
+
+export const core: PresetProperty<'core', StorybookConfig> = async (config, options) => {
+  const framework = await options.presets.apply<StorybookConfig['framework']>('framework');
+
+  return {
+    ...config,
+    builder: {
+      name: dirname(
+        require.resolve(join('@storybook/builder-webpack5', 'package.json'))
+      ) as '@storybook/builder-webpack5',
+      options: typeof framework === 'string' ? {} : framework.options.builder || {},
+    },
+  };
+};
+
+export const config: StorybookConfig['previewAnnotations'] = (entry = []) => [
+  ...entry,
+  require.resolve('./preview'),
+];
+
+export const babel = async (config: TransformOptions): Promise<TransformOptions> =>
+  configureStyledJsxTransforms(config);
+
+export const webpack: StorybookConfig['webpack'] = async (baseConfig, options) => {
+  const { nextConfigPath } = options as FrameworkOptions;
+  const nextConfig = await configureConfig(baseConfig, nextConfigPath);
+
+  configureRuntimeNextjsVersionResolution(baseConfig);
+  configureImports(baseConfig);
+  configureCss(baseConfig, nextConfig);
+  configureImages(baseConfig);
+  configureRouting(baseConfig);
+  configureStyledJsx(baseConfig);
+
+  return baseConfig;
+};

code/frameworks/nextjs/src/types.ts
@@ -1,0 +1,48 @@
+import type {
+  ReactOptions,
+  StorybookConfig as StorybookConfigBase,
+  TypescriptOptions as TypescriptOptionsReact,
+} from '@storybook/preset-react-webpack';
+import type {
+  StorybookConfigWebpack,
+  BuilderOptions,
+  TypescriptOptions as TypescriptOptionsBuilder,
+} from '@storybook/builder-webpack5';
+
+type FrameworkName = '@storybook/cra';
+type BuilderName = '@storybook/builder-webpack5';
+
+export type FrameworkOptions = ReactOptions & {
+  nextConfigPath?: string;
+
+  builder?: BuilderOptions;
+};
+
+type StorybookConfigFramework = {
+  framework:
+    | FrameworkName
+    | {
+        name: FrameworkName;
+        options: FrameworkOptions;
+      };
+  core?: StorybookConfigBase['core'] & {
+    builder?:
+      | BuilderName
+      | {
+          name: BuilderName;
+          options: BuilderOptions;
+        };
+  };
+  typescript?: Partial<TypescriptOptionsBuilder & TypescriptOptionsReact> &
+    StorybookConfigBase['typescript'];
+};
+
+/**
+ * The interface for Storybook configuration in `main.ts` files.
+ */
+export type StorybookConfig = Omit<
+  StorybookConfigBase,
+  keyof StorybookConfigWebpack | keyof StorybookConfigFramework
+> &
+  StorybookConfigWebpack &
+  StorybookConfigFramework;

visveswaran24/storybook-

Co-authored-by: Yann Braga <yannbf@gmail.com>

code/frameworks/nextjs/src/preset.ts
@@ -1,0 +1,86 @@
+// https://storybook.js.org/docs/react/addons/writing-presets
+import { dirname, join } from 'path';
+import { Options, PresetProperty } from '@storybook/core-common';
+import { TransformOptions } from '@babel/core';
+import { configureConfig } from './config/webpack';
+import { configureCss } from './css/webpack';
+import { configureImports } from './imports/webpack';
+import { configureRouting } from './routing/webpack';
+import { configureStyledJsx } from './styledJsx/webpack';
+import { configureStyledJsxTransforms } from './styledJsx/babel';
+import { configureImages } from './images/webpack';
+import { configureRuntimeNextjsVersionResolution } from './utils';
+import { FrameworkOptions, StorybookConfig } from './types';
+
+export const addons: PresetProperty<'addons', StorybookConfig> = [
+  dirname(require.resolve(join('@storybook/preset-react-webpack', 'package.json'))),
+  dirname(require.resolve(join('@storybook/react', 'package.json'))),
+  dirname(require.resolve(join('@storybook/builder-webpack5', 'package.json'))),
+];
+
+const defaultFrameworkOptions: FrameworkOptions = {};
+
+export const frameworkOptions = async (
+  _: never,
+  options: Options
+): Promise<StorybookConfig['framework']> => {
+  const config = await options.presets.apply<StorybookConfig['framework']>('framework');
+
+  if (typeof config === 'string') {
+    return {
+      name: config,
+      options: defaultFrameworkOptions,
+    };
+  }
+  if (typeof config === 'undefined') {
+    return {
+      name: require.resolve('@storybook/nextjs') as '@storybook/nextjs',
+      options: defaultFrameworkOptions,
+    };
+  }
+
+  return {
+    name: config.name,
+    options: {
+      ...defaultFrameworkOptions,
+      ...config.options,
+    },
+  };
+};
+
+export const core: PresetProperty<'core', StorybookConfig> = async (config, options) => {
+  const framework = await options.presets.apply<StorybookConfig['framework']>('framework');
+
+  return {
+    ...config,
+    builder: {
+      name: dirname(
+        require.resolve(join('@storybook/builder-webpack5', 'package.json'))
+      ) as '@storybook/builder-webpack5',
+      options: typeof framework === 'string' ? {} : framework.options.builder || {},
+    },
+  };
+};
+
+export const config: StorybookConfig['previewAnnotations'] = (entry = []) => [
+  ...entry,
+  require.resolve('@storybook/nextjs/preview.js'),
+];
+
+export const babel = async (config: TransformOptions): Promise<TransformOptions> =>
+  configureStyledJsxTransforms(config);
+
+export const webpackFinal: StorybookConfig['webpackFinal'] = async (baseConfig, options) => {
+  const frameworkOptions = await options.presets.apply<FrameworkOptions>('frameworkOptions');
+  const { nextConfigPath } = frameworkOptions;
+  const nextConfig = await configureConfig(baseConfig, nextConfigPath);
+
+  configureRuntimeNextjsVersionResolution(baseConfig);
+  configureImports(baseConfig);
+  configureCss(baseConfig, nextConfig);
+  configureImages(baseConfig);
+  configureRouting(baseConfig);
+  configureStyledJsx(baseConfig);
+
+  return baseConfig;
+};

code/frameworks/nextjs/src/types.ts
@@ -1,0 +1,48 @@
+import type {
+  ReactOptions,
+  StorybookConfig as StorybookConfigBase,
+  TypescriptOptions as TypescriptOptionsReact,
+} from '@storybook/preset-react-webpack';
+import type {
+  StorybookConfigWebpack,
+  BuilderOptions,
+  TypescriptOptions as TypescriptOptionsBuilder,
+} from '@storybook/builder-webpack5';
+
+type FrameworkName = '@storybook/nextjs';
+type BuilderName = '@storybook/builder-webpack5';
+
+export type FrameworkOptions = ReactOptions & {
+  nextConfigPath?: string;
+
+  builder?: BuilderOptions;
+};
+
+type StorybookConfigFramework = {
+  framework:
+    | FrameworkName
+    | {
+        name: FrameworkName;
+        options: FrameworkOptions;
+      };
+  core?: StorybookConfigBase['core'] & {
+    builder?:
+      | BuilderName
+      | {
+          name: BuilderName;
+          options: BuilderOptions;
+        };
+  };
+  typescript?: Partial<TypescriptOptionsBuilder & TypescriptOptionsReact> &
+    StorybookConfigBase['typescript'];
+};
+
+/**
+ * The interface for Storybook configuration in `main.ts` files.
+ */
+export type StorybookConfig = Omit<
+  StorybookConfigBase,
+  keyof StorybookConfigWebpack | keyof StorybookConfigFramework
+> &
+  StorybookConfigWebpack &
+  StorybookConfigFramework;